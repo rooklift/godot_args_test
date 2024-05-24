c74b5ce344c1c970004a4b57e7d676accdbdf695
rooklift <16438795+rooklift@users.noreply.github.com>
2024-05-24 12:56:56 +0100

    Update Root.gd

--- Root.gd
+++ Root.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 
 func _ready() -> void:
-	self.function_that_works([123.4])		# Fine
-	$Foo.function_that_crashes([123.4])		# Invalid type error (but why??)
+	self.function_that_works([123.4])							# Fine
+	self.get_node("Foo").function_that_crashes([123.4])			# Invalid type error (but why??)
 	
-	
+
 func function_that_works(arr: Array[float]) -> void:
 	print(arr)
